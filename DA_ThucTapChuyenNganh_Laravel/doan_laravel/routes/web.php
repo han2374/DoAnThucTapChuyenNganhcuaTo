99b923d93c3afc0a91700a5978bd7e0bb63708c1
han2374 <thienhav3011@gmail.com>
2025-11-09 23:55:34 +0700

    Nop do an thuc tap chuyen nganh tuan 3

---
+++ DA_ThucTapChuyenNganh_Laravel/doan_laravel/routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('index');
+})->name('home');
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+Route::get('/portfolio', function () {
+    return view('portfolio');
+})->name('portfolio');
+Route::get('/services', function () {
+    return view('services');
+})->name('services');
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact');
+Route::get('/blog', function () {
+    return view('blog');
+})->name('blog');
+Route::get('/blog-details', function () {
+    return view('blog-details');
+})->name('blog-details');
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+
+
